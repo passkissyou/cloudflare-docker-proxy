67f32e85750b42785ebbff160eb6f61cf414f18b
passkissyou <74122334+passkissyou@users.noreply.github.com>
2024-06-25 23:06:03 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.passkissyou.com": dockerHub,
+  "quay.passkissyou.com": "https://quay.io",
+  "gcr.passkissyou.com": "https://gcr.io",
+  "k8s-gcr.passkissyou.com": "https://k8s.gcr.io",
+  "k8s.passkissyou.com": "https://registry.k8s.io",
+  "ghcr.passkissyou.com": "https://ghcr.io",
+  "cloudsmith.passkissyou.com": "https://docker.cloudsmith.io",
+  "ecr.passkissyou.com": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.passkissyou.com": dockerHub,
 };
 
 function routeByHosts(host) {
